231ce1538be679f8771af54ccabe3a8b02fa7069
Denisa Dupu <dupudenisa@gmail.com>
2020-04-02 00:24:53 -0500

    some updates

--- src/components/EmployeeDash.js
+++ src/components/EmployeeDash.js
@@ -1,15 +1,62 @@
 import React, { Component } from "react";
-
+import API from "../utils/API";
 
 
 class EmployeeDash extends Component {
   state = {
     search: "",
-    result: {}
+    result: {},
+    filter: []
+  };
+
+  componentDidMount() {
+    this.searchEmployees();
+  }
+
+  searchEmployees = query => {
+    API.search(query)
+      .then(res => this.setState({ result: res.data }))
+      .catch(err => console.log(err));
+  };
+  handleInputChange = event => {
+    const value = event.target.value;
+    
+    this.setState({
+      filter: ({filtered})
+    });
   };
   render(){
     return(
-      <h1>This is the Dashboard</h1>
+      <Container>
+        <Row>
+          <Col size="md-8">
+            <Card
+              heading={this.state.result.Name || "Search for a employee"}
+            >
+              {this.state.result.Title ? (
+                <EmployeeInfo
+                  name={this.state.result.Name}
+                  src={this.state.result.Poster}
+                  
+                />
+              ) : (
+                <h3>No Results to Display</h3>
+              )}
+            </Card>
+          </Col>
+          <Col size="md-4">
+            <Card heading="Search">
+              <SearchEmployee
+                value={this.state.search}
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}
+              />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+      
+      
     )
   }
 }
