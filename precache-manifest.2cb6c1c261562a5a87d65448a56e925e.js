53bd52dfa3b6de32808d6e3850865b309a16c209
Denisa Dupu <dupudenisa@gmail.com>
2020-04-01 19:42:24 -0500

    Updates

---
+++ precache-manifest.2cb6c1c261562a5a87d65448a56e925e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2c681ee16011cdec6d6382f568d8d7",
+    "url": "/dashboard-app/index.html"
+  },
+  {
+    "revision": "5ff5b0813952f38b7ad0",
+    "url": "/dashboard-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3e534a51db3507bbcd43",
+    "url": "/dashboard-app/static/js/2.cc196b77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dashboard-app/static/js/2.cc196b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ff5b0813952f38b7ad0",
+    "url": "/dashboard-app/static/js/main.e9e782a6.chunk.js"
+  },
+  {
+    "revision": "e84ae0e5b48598fdfece",
+    "url": "/dashboard-app/static/js/runtime-main.a7dcf191.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashboard-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
