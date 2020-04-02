2d5e9884a98dac0775a6bdd99cf1342f08e9b0f1
Denisa Dupu <dupudenisa@gmail.com>
2020-04-01 21:51:20 -0500

    Created Dashboard Page

---
+++ src/components/EmployeeDash.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+
+class EmployeeDash extends Component {
+  state = {
+    search: "",
+    result: {}
+  };
+  render(){
+    return(
+      <h1>This is the Dashboard</h1>
+    )
+  }
+}
+
+
+export default EmployeeDash;
